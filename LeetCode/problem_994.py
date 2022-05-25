47e97705592f91c8dbe0bf954a14c81f53c6855e
giraycoskun <giraycoskun@sabanciuniv.edu>
2022-05-25 21:25:06 +0300

    update: fix the problem of rotting oranges

--- LeetCode/problem_994.py
+++ LeetCode/problem_994.py
@@ -10,15 +10,59 @@ from helper import *
 def orangesRotting(grid: list[list[int]]) -> int:
     r = len(grid)
     c = len(grid[0])
-    max_time = 0
+    rotten_queue = []
+    oranges = set()
     for i in range(r):
         for j in range(c):
             if grid[i][j] == 2:
-                queue = []
-                while
+                rotten_queue.append((i, j))
+            if grid[i][j] == 1:
+                oranges.add((i, j))
+
+    if len(oranges) == 0:
+        return 0
+
+    level = 0
+    while len(rotten_queue) != 0:
+        level_size = len(rotten_queue)
+        while level_size != 0:
+            level_size -= 1
+            i, j = rotten_queue.pop(0)
+            if i + 1 < r:
+                item = (i + 1, j)
+                if item in oranges:
+                    rotten_queue.append(item)
+                    oranges.discard(item)
+            if i - 1 >= 0:
+                item = (i - 1, j)
+                if item in oranges:
+                    rotten_queue.append(item)
+                    oranges.discard(item)
+
+            if j + 1 < c:
+                item = (i, j + 1)
+                if item in oranges:
+                    rotten_queue.append(item)
+                    oranges.discard(item)
+
+            if j - 1 >= 0:
+                item = (i, j - 1)
+                if item in oranges:
+                    rotten_queue.append(item)
+                    oranges.discard(item)
+        level += 1
+
+    if len(oranges) != 0:
+        return -1
+    else:
+        return level-1
 
 
 if __name__ == '__main__':
     grid = [[2, 1, 1], [0, 1, 1], [1, 0, 1]]
     test = orangesRotting(grid)
     print(f"TEST 1: {test}")
+
+    grid = [[2, 1, 1], [1, 1, 0], [0, 1, 1]]
+    test = orangesRotting(grid)
+    print(f"TEST 2: {test}")
